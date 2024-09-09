0ac1418702f269a7360a355b97b79db6a057e009
Johora99 <fatimatuzjohora1999@gmail.com>
2024-09-09 14:02:17 +0600

    number looping

---
+++ problem_1.js
@@ -0,0 +1,17 @@
+// 1-10 number loop..............
+
+
+//By while loop====================
+let num = 1;
+while(num <=10){
+    console.log(num);
+    num++;
+}
+
+
+// By for loop ==========================
+
+
+for(i = 1; i <= 10; i++){
+    console.log(i);
+}
