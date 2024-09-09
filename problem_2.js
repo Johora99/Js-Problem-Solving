3626d9c9e6545b0fa3930f70405c42f855b5a53d
Johora99 <fatimatuzjohora1999@gmail.com>
2024-09-09 18:40:50 +0600

    sum of numbers

---
+++ problem_2.js
@@ -0,0 +1,19 @@
+// sum of 1-10 ============
+
+
+
+// by using while loop==================
+let x = 0;
+let i = 1;
+while (i <=10){
+    x += i;
+    i++;
+}
+console.log("sum :", x)
+
+// by using for loop======================
+let sum = 0;
+for (i = 1; i <=10; i++){
+    sum += i;
+}
+console.log("sum :", sum);
---
+++ problem_3.js
@@ -0,0 +1,42 @@
+// sum of odd numbers 1-20===============
+
+let sumOdd = 0;
+let num = 0;
+while(num < 20){
+    if(num % 2 !== 0){
+        sumOdd += num;
+    }
+    num++;
+}
+console.log(sumOdd);
+
+
+let sumOdd2 = 0;
+for (i = 0; i <20; i++){
+    if (i % 2 !==0){
+        sumOdd2 += i;
+    }
+}
+console.log(sumOdd2);
+
+// sum of even number 1-20=================
+
+let sumEven = 0;
+let number = 1;
+while(number < 20){
+    if (number % 2 ===0){
+        sumEven += number;
+    }
+    number++;
+}
+console.log(sumEven);
+
+let sumEven2 = 0;
+
+for(number = 2; number < 20; number++){
+    if ( number % 2 ===0){
+        sumEven2 += number;
+    }
+    
+}
+console.log(sumEven2);
